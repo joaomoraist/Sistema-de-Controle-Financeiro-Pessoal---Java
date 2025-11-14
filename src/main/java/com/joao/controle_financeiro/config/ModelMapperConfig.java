3b00bae0b1a90e2a66bf5f022a26337563d4c555
João Morais <jv.timotio@gmail.com>
2025-11-14 11:19:27 -0300

    Atualização do projeto

---
+++ src/main/java/com/joao/controle_financeiro/config/ModelMapperConfig.java
@@ -0,0 +1,4 @@
+package com.joao.controle_financeiro.config;
+
+public class ModelMapperConfig {
+}
---
+++ src/main/java/com/joao/controle_financeiro/dto/UsuarioResponseDTO.java
@@ -0,0 +1,4 @@
+package com.joao.controle_financeiro.dto;
+
+public class UsuarioResponseDTO {
+}
